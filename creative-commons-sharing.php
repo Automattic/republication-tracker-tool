bd01d6ff26f3452ddd044c31161ca5ae85e2f707
rclations <contact@rclations.com>
2017-10-03 16:01:12 -0400

    adding initial wp-cli plugin scaffolding

---
+++ creative-commons-sharing.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name:     Creative Commons Sharing
+ * Description:     Allow readers to share your content via a creative commons license.
+ * Author:          INN Labs
+ * Author URI:      https://labs.inn.org
+ * Text Domain:     creative-commons-sharing
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Creative_Commons_Sharing
+ */
