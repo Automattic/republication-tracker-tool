77011306a1bc327b86f48ce4253505542cd1a40d
rclations <contact@rclations.com>
2017-10-11 15:32:03 -0400

    update widget styling - fixes #5

--- includes/class-widget.php
+++ includes/class-widget.php
@@ -60,7 +60,7 @@
 
 		echo '<div class="license">';
 			echo '<p><a href="#TB_inline?width=600&height=550&inlineId=creative-commons-share-modal" class="creative-commons-button thickbox">Republish this article</a></p>';
-			echo '<p><a rel="license" href="http://creativecommons.org/licenses/by-nd/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nd/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nd/4.0/">Creative Commons Attribution-NoDerivatives 4.0 International License</a>.</p>';
+			echo '<p><a class="license" rel="license" href="http://creativecommons.org/licenses/by-nd/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nd/4.0/88x31.png" /></a></p>';
 		echo '</div>';
 
 		echo sprintf( '<div class="message">%s</div>', wpautop( esc_html__( $instance['text'], 'creative-commons-sharing' ) ) );
