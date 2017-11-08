1477937c8bb01f5961b27080aaef5f01ee7e62f2
rclations <contact@rclations.com>
2017-11-08 17:28:08 -0500

    bugsquish for article view counts

--- includes/pixel.php
+++ includes/pixel.php
@@ -6,11 +6,16 @@ if ( isset( $_GET['post'] ) && isset( $_GET['url'] ) ) {
 	$post_id = absint( $_GET['post'] );
 	$url = esc_url_raw( $_GET['url'] );
 	$value = get_post_meta( $post_id, 'creative_commons_sharing', true );
-	if ( isset( $value[ $url ] ) ) {
-		$value[ $url ]++;
+	if ( $value ) {
+		if ( isset( $value[ $url ] ) ) {
+			$value[ $url ]++;
+		} else {
+			$value[ $url ] = 1;
+		}
 	} else {
-		$value[ $url ] = 1;
+		$value = array(
+			$url => 1
+		);
 	}
-
 	$update = update_post_meta( $post_id, 'creative_commons_sharing', $value );
 }
