261bd11b82f61943eb648de9d1faa5671c0233aa
rclations <contact@rclations.com>
2017-11-08 16:29:18 -0500

    switch tracking code, store pages hits come from

--- includes/pixel.php
+++ includes/pixel.php
@@ -1,16 +1,16 @@
 <?php
-require_once("../../../../wp-load.php");
+require_once( '../../../../wp-load.php' );
 
-if ( isset( $_GET['post'] ) ) {
+if ( isset( $_GET['post'] ) && isset( $_GET['url'] ) ) {
 
 	$post_id = absint( $_GET['post'] );
-	$value = intval( get_post_meta( $post_id, 'creative_commons_sharing', true ) ) + 1;
+	$url = esc_url_raw( $_GET['url'] );
+	$value = get_post_meta( $post_id, 'creative_commons_sharing', true );
+	if ( isset( $value[ $url ] ) ) {
+		$value[ $url ]++;
+	} else {
+		$value[ $url ] = 1;
+	}
 
 	$update = update_post_meta( $post_id, 'creative_commons_sharing', $value );
 }
-
-// Output the image
-header('Content-Type: image/gif');
-
-// This echo is equivalent to read an image, readfile('pixel.gif')
-echo "\x47\x49\x46\x38\x37\x61\x1\x0\x1\x0\x80\x0\x0\xfc\x6a\x6c\x0\x0\x0\x2c\x0\x0\x0\x0\x1\x0\x1\x0\x0\x2\x2\x44\x1\x0\x3b";
