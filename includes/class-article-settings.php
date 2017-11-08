a42ca6c0ed652ce6a99854fab2751a9e124b2a18
rclations <contact@rclations.com>
2017-11-08 16:45:59 -0500

    updating post stats display

--- includes/class-article-settings.php
+++ includes/class-article-settings.php
@@ -53,7 +53,7 @@ class Creative_Commons_Sharing_Article_Settings {
 			esc_html__( 'Creative Commons Sharing', 'creative-commons-sharing' ),
 			array( $this, 'render_metabox' ),
 			array( 'post', 'page' ),
-			'side',
+			'advanced',
 			'default'
 		);
 	}
@@ -66,6 +66,25 @@ class Creative_Commons_Sharing_Article_Settings {
 	 * @param obj $args Arguments object.
 	 */
 	public function render_metabox( $post, $args ) {
-		echo sprintf( 'This article has been shared %d times.', intval( get_post_meta( $post->ID, 'creative_commons_sharing', true ) ) );
+		$shares = get_post_meta( $post->ID, 'creative_commons_sharing', true );
+		echo '<table class="wp-list-table widefat fixed striped posts">';
+			echo '<thead>';
+				echo '<th scope="col" id="url" class="manage-column column-primary"><span>URL</span><span class="sorting-indicator"></span></th>';
+				echo '<th scope="col" id="views" class="manage-column "># of Views</th>';
+			echo '</thead>';
+			echo '<tbody id="the-list">';
+				foreach ( $shares as $url => $count ) {
+					echo sprintf(
+						'<tr><td class="column-primary" data-colname="URL"><a href="%1$s" target="_blank">%1$s</a></td><td class="views" data-colname="Views">%2$s</td></tr>',
+						$url,
+						$count
+					);
+				}
+			echo '</tbody>';
+			echo '<tfoot>';
+				echo '<th scope="col" id="url" class="manage-column column-primary"><span>URL</span><span class="sorting-indicator"></span></th>';
+				echo '<th scope="col" id="views" class="manage-column"># of Views</th>';
+			echo '</tfoot>';
+		echo '</table>';
 	}
 }
