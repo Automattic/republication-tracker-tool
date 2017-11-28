a442c324077aa3f42df69f6ce240889cd56247fe
rclations <contact@rclations.com>
2017-11-28 18:06:51 -0500

    move content generation over to separate file to be loaded via ajax - wip

--- includes/class-widget.php
+++ includes/class-widget.php
@@ -51,54 +51,7 @@
 		$pixel = sprintf( '<script type="text/javascript" id="creative-commons-sharing-source" src="%s" data-postid="%s" data-pluginsdir="%s" async="true"></script>', plugins_url( 'assets/pixel.js', dirname( __FILE__ ) ), $post->ID, plugins_url() );
 		$license_statement = get_option( 'creative_commons_sharing_policy' );
 
-		$content = $post->post_content;
-		global $shortcode_tags;
-		if ( is_array( $shortcode_tags ) ) {
-			foreach ( $shortcode_tags as $tag ) {
-				$content = str_replace( $tag, '', $content );
-			}
-		}
-
-		// remove images
-		$content = preg_replace( "/<img[^>]+\>/i", " ", $content );
-
-		// force UTF-8
-		$content = htmlspecialchars( $content, ENT_HTML5, 'UTF-8', true );
-
-		echo '<div id="creative-commons-share-modal" style="display:none;">';
-			echo '<div id="creative-commons-share-modal-content">';
-				echo '<div class="creative-commons-close">X</div>';
-				echo sprintf( '<h2>%s</h2>', esc_html__( 'Republish this article', 'creative-commons-sharing' ) );
-				echo '<div class="cc-policy">';
-					echo '<div class="cc-license">';
-						echo sprintf( '<a rel="license" target="_blank" href="http://creativecommons.org/licenses/by-nd/4.0/"><img alt="%s" style="border-width:0" src="https://i.creativecommons.org/l/by-nd/4.0/88x31.png" /></a>', esc_html__( 'Creative Commons License' ) );
-						echo wpautop(
-							sprintf(
-								__( 'This work is licensed under a <a rel="license" target="_blank" href="%s">Creative Commons Attribution-NoDerivatives 4.0 International License</a>.' ),
-								'http://creativecommons.org/licenses/by-nd/4.0/'
-							)
-						);
-					echo '</div>';
-					echo $license_statement;
-				echo '</div>';
-				echo '<div class="article-info">';
-					echo sprintf(
-						'<h1>%s</h1><p class="byline">%s <br />%s</p>',
-						$post->post_title,
-						esc_html__( 'by', 'creative-commons-sharing' ) . ' ' . get_the_author_meta( 'display_name', $post->post_author ) . ', ' . get_bloginfo( 'name' ),
-						date( 'F j, Y', strtotime( $post->post_date ) )
-					);
-				echo '</div>';
-				echo sprintf( '
-					<textarea id="creative-commons-shareable-content" rows="5"><h1>%s</h1><p class="byline">%s <br />%s</p>%s</textarea>',
-					$post->post_title,
-					esc_html__( 'by', 'creative-commons-sharing' ) . ' ' . get_the_author_meta( 'display_name', $post->post_author ),
-					get_bloginfo( 'name' ) . ', ' . date( 'F j, Y', strtotime( $post->post_date ) ),
-					wpautop( $content . "\n\n" . $attribution_statement . $pixel )
-				);
-				echo wpautop( sprintf( '<button onclick="copyToClipboard(\'#creative-commons-shareable-content\')">%s</button>', esc_html__( 'Copy to Clipboard', 'creative-commons-sharing' ) ) );
-			echo '</div>';
-		echo '</div>';
+		echo '<div id="creative-commons-share-modal" style="display:none;"></div>';
 
 		echo '<div class="license">';
 			echo sprintf(
