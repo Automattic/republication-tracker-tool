cef28aa8553e32a7f498141ae33b0173c2f44732
rclations <contact@rclations.com>
2017-11-29 13:23:44 -0500

    make vars more clear

--- assets/pixel.js
+++ assets/pixel.js
@@ -5,9 +5,9 @@
     loc = window.location;
 	currentUrl = encodeURIComponent( window.location.protocol + "//" + window.location.host + window.location.pathname );
 	postId = document.getElementById( 'creative-commons-sharing-source' ).getAttribute( 'data-postid' );
-	homeUrl = document.getElementById( 'creative-commons-sharing-source' ).getAttribute( 'data-pluginsdir' );
+	pluginsdir = document.getElementById( 'creative-commons-sharing-source' ).getAttribute( 'data-pluginsdir' );
     img = document.createElement( 'img' );
-    img.setAttribute( 'src', homeUrl + '/creative-commons-sharing/includes/pixel.php?post=' + postId + '&url=' + currentUrl );
+    img.setAttribute( 'src', pluginsdir + '/creative-commons-sharing/includes/pixel.php?post=' + postId + '&url=' + currentUrl );
     img.setAttribute( 'width', '1' );
     img.setAttribute( 'height', '1' );
     document.body.appendChild( img );
