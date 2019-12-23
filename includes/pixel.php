973cc517676016c0c76cdb6c9b3cfc2d602e62c7
Josh Darby <josh@inn.org>
2019-12-23 11:44:47 -0500

    Update $site_icon_url to 50px

--- includes/pixel.php
+++ includes/pixel.php
@@ -108,6 +108,6 @@ if ( isset( $_GET['post'] ) ) {
 }
 
 // grab our site icon and redirect to it once the script finishes
-$site_icon_url = get_site_icon_url( 'thumb' );
+$site_icon_url = get_site_icon_url( 50 );
 wp_safe_redirect($site_icon_url, 303);
 exit;
