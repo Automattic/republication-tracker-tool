710fd0394c49d2c0e35aed75b8908a297a9680cc
Jonathan Stegall <jstegall@minnpost.com>
2020-03-10 12:18:30 -0500

    show the modal if the url hash is #show-republish. also adds a close handler if user presses escape key

--- assets/widget.js
+++ assets/widget.js
@@ -20,29 +20,51 @@ function modal_actions(){
 
 	// Responsive modal
 	var $modal = $('#republication-tracker-tool-modal');
-	var $modal_content = $('#republication-tracker-tool-modal-content');
-	var $btn = $('.republication-tracker-tool-button');
+	var $btn = $('#cc-btn');
 	var $close = $('.republication-tracker-tool-close');
 
+	// url hash of #show-republish: open the modal
+	if ( '#show-republish' === window.location.hash ) {
+		show_modal( $modal );
+	}
+
+	// click the republish button: open the modal
 	$btn.click(function(){
-		//$modal.html( html );
-		$modal.show();
-		$modal_content.show();
-		$('body').addClass('modal-open-disallow-scrolling');
-		$('#republication-tracker-tool-modal-content').unbind().click(function(e) {
-			e.stopPropagation();
-		});
+		show_modal( $modal );
 	});
 
+	// click on the modal: close the modal
 	$modal.click(function(){
-		$('body').removeClass('modal-open-disallow-scrolling');
-		$modal.hide();
+		close_modal( $modal );
 	});
 
+	// close button click: close the modal
 	$close.click(function(){
-		$('body').removeClass('modal-open-disallow-scrolling');
-		$modal.hide();
+		close_modal( $modal );
 	});
+
+	// escape key press: close the modal
+	$(document).keyup(function(e) {
+		if (27 === e.keyCode) {
+			close_modal( $modal );
+		}
+	});
+}
+
+function show_modal( $modal ) {
+	var $modal_content = $('#republication-tracker-tool-modal-content');
+	//$modal.html( html );
+	$modal.show();
+	$modal_content.show();
+	$('body').addClass('modal-open-disallow-scrolling');
+	$('#republication-tracker-tool-modal-content').unbind().click(function(e) {
+		e.stopPropagation();
+	});
+}
+
+function close_modal( $modal ) {
+	$('body').removeClass('modal-open-disallow-scrolling');
+	$modal.hide();
 }
 
 jQuery(document).ready(function(){
