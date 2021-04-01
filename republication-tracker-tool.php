2a922eb4a69721b230434b70f105abf0b6b092dd
matticbot <semantic-release-bot@martynus.net>
2021-04-01 09:41:47 +0000

    chore(release): 1.1.0-alpha.1 [skip ci]
    # [1.1.0-alpha.1](https://github.com/Automattic/republication-tracker-tool/compare/v1.0.2...v1.1.0-alpha.1) (2021-04-01)
    
    ### Features
    
    * add AMP support ([#93](https://github.com/Automattic/republication-tracker-tool/issues/93)) ([c847f0a](https://github.com/Automattic/republication-tracker-tool/commit/c847f0a95c9cdb9c0bd089ef45880e56867ffe1b)), closes [#57](https://github.com/Automattic/republication-tracker-tool/issues/57)

--- republication-tracker-tool.php
+++ republication-tracker-tool.php
@@ -6,7 +6,7 @@
  * Author URI:      https://labs.inn.org
  * Text Domain:     republication-tracker-tool
  * Domain Path:     /languages
- * Version:         1.0.0
+ * Version:         1.1.0-alpha.1
  *
  * @package         Republication_Tracker_Tool
  */
