c18084ff0cb573d13652c8d0884abf069ef99206
Laurel Fulford <laurel.fulford@automattic.com>
2023-12-14 11:34:15 -0800

    feat: accessibility improvements to republication tracker modal

--- assets/widget.js
+++ assets/widget.js
@@ -22,36 +22,37 @@ function modal_actions(){
 	var $modal = $('#republication-tracker-tool-modal');
 	var $btn = $('.republication-tracker-tool-button');
 	var $close = $('.republication-tracker-tool-close');
+	var $copy = $( '#republication-copy-to-clipboard' );
 
 	// url hash of #show-republish: open the modal
 	if ( '#show-republish' === window.location.hash ) {
-		show_modal( $modal );
+		show_modal( $modal, $close );
 	}
 
 	// click the republish button: open the modal
 	$btn.click(function(){
-		show_modal( $modal );
+		show_modal( $modal, $close );
 	});
 
 	// click on the modal: close the modal
 	$modal.click(function(){
-		close_modal( $modal );
+		close_modal( $modal, $btn );
 	});
 
 	// close button click: close the modal
 	$close.click(function(){
-		close_modal( $modal );
+		close_modal( $modal, $btn );
 	});
 
 	// escape key press: close the modal
 	$(document).keyup(function(e) {
 		if (27 === e.keyCode) {
-			close_modal( $modal );
+			close_modal( $modal, $btn );
 		}
 	});
 }
 
-function show_modal( $modal ) {
+function show_modal( $modal, $close ) {
 	var $ = jQuery;
 	var $modal_content = $('#republication-tracker-tool-modal-content');
 	//$modal.html( html );
@@ -61,12 +62,41 @@ function show_modal( $modal ) {
 	$('#republication-tracker-tool-modal-content').unbind().click(function(e) {
 		e.stopPropagation();
 	});
+	trapFocus( $modal );
+	$close.focus();
 }
 
-function close_modal( $modal ) {
+function trapFocus( $modal ) {
+	var $ = jQuery;
+	var focusableEls = $modal.find('a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input[type="text"]:not([disabled]), input[type="radio"]:not([disabled]), input[type="checkbox"]:not([disabled]), select:not([disabled])');
+	var firstFocusableEl = focusableEls[0];
+	var lastFocusableEl = focusableEls[focusableEls.length - 1];
+	var KEYCODE_TAB = 9;
+
+	$modal.on( 'keydown', function( e ) {
+		var isTabPressed = ( e.key === 'Tab' || e.keyCode === KEYCODE_TAB );
+
+		if ( ! isTabPressed ) {
+			return;
+		}
+
+		if ( e.shiftKey ) /* shift + tab */ {
+			if ( document.activeElement === firstFocusableEl ) {
+				lastFocusableEl.focus();
+				e.preventDefault();
+			}
+		} else if ( document.activeElement === lastFocusableEl ) {
+			firstFocusableEl.focus();
+			e.preventDefault();
+		}
+	} );
+}
+
+function close_modal( $modal, $btn ) {
 	var $ = jQuery;
 	$('body').removeClass('modal-open-disallow-scrolling');
 	$modal.hide();
+	$btn.focus();
 }
 
 jQuery(document).ready(function(){
