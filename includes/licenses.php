03bc4d47e7991b2da9e47db31f14df828ff1b27a
Allyson Souza <2000638+allysonsouza@users.noreply.github.com>
2025-01-30 10:41:35 -0300

    feat: add other cc licenses to the options

--- includes/licenses.php
+++ includes/licenses.php
@@ -1,13 +1,38 @@
 <?php
 const REPUBLICATION_TRACKER_TOOL_LICENSES = [
-		'cc-by-nd-4.0' => [
-			'label' => 'CC BY-ND 4.0',
-			'description' => 'Creative Commons Attribution-NoDerivatives 4.0 International License',
-			'url' => 'https://creativecommons.org/licenses/by-nd/4.0/'
-		],
-		'cc-by-nc-nd-4.0' => [
-			'label' => 'CC BY-NC-ND 4.0',
-			'description' => 'Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License',
-			'url' => 'https://creativecommons.org/licenses/by-nc-nd/4.0/'
-		],
-	];
+	'cc-by' => [
+		'label' => 'CC BY 4.0',
+		'description' => 'Creative Commons Attribution 4.0 International License',
+		'url' => 'https://creativecommons.org/licenses/by/4.0/'
+	],
+	'cc-by-sa' => [
+		'label' => 'CC BY-SA 4.0',
+		'description' => 'Creative Commons Attribution-ShareAlike 4.0 International License',
+		'url' => 'https://creativecommons.org/licenses/by-sa/4.0/'
+	],
+	'cc-by-nc' => [
+		'label' => 'CC BY-NC 4.0',
+		'description' => 'Creative Commons Attribution-NonCommercial 4.0 International License',
+		'url' => 'https://creativecommons.org/licenses/by-nc/4.0/'
+	],
+	'cc-by-nc-sa' => [
+		'label' => 'CC BY-NC-SA 4.0',
+		'description' => 'Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License',
+		'url' => 'https://creativecommons.org/licenses/by-nc-sa/4.0/'
+	],
+	'cc-by-nd-4.0' => [
+		'label' => 'CC BY-ND 4.0',
+		'description' => 'Creative Commons Attribution-NoDerivatives 4.0 International License',
+		'url' => 'https://creativecommons.org/licenses/by-nd/4.0/'
+	],
+	'cc-by-nc-nd-4.0' => [
+		'label' => 'CC BY-NC-ND 4.0',
+		'description' => 'Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License',
+		'url' => 'https://creativecommons.org/licenses/by-nc-nd/4.0/'
+	],
+	'cc-zero-1.0' => [
+		'label' => 'CC0 1.0',
+		'description' => 'CC0 1.0 Universal',
+		'url' => 'https://creativecommons.org/publicdomain/zero/1.0/'
+	],
+];
