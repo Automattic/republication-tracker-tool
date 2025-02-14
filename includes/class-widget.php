9a1ef6285c100a76ba70879ae2a2da3be9b611f6
Allyson Souza <2000638+allysonsouza@users.noreply.github.com>
2025-02-14 15:05:10 -0300

    fix: make modal the default selected option of nothing already save on widget options

--- includes/class-widget.php
+++ includes/class-widget.php
@@ -132,7 +132,7 @@ class Republication_Tracker_Tool_Widget extends WP_Widget {
 		echo sprintf( '<p><em>%s</em></p>', esc_html__( 'This widget will only display on single articles.', 'republication-tracker-tool' ) );
 		$title  = ! empty( $instance['title'] )  ? $instance['title'] : '';
 		$text   = ! empty( $instance['text'] )   ? $instance['text'] : esc_html__( 'Republish our articles for free, online or in print, under a Creative Commons license.', 'republication-tracker-tool' );
-		$layout = ! empty( $instance['layout'] ) ? $instance['layout'] : ''
+		$layout = ! empty( $instance['layout'] ) ? $instance['layout'] : 'modal'
 		?>
 		<p>
 			<label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_attr_e( 'Title:', 'republication-tracker-tool' ); ?></label>
