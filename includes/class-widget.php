b053756158c4b2185ef49f653065ae9c723d4d86
Allyson Souza <2000638+allysonsouza@users.noreply.github.com>
2025-02-20 13:33:15 -0300

    fix: add role="link" to button that points to republish page

--- includes/class-widget.php
+++ includes/class-widget.php
@@ -76,7 +76,7 @@ class Republication_Tracker_Tool_Widget extends WP_Widget {
 
 		if ( 'page' === $instance['layout'] ) {
 			echo sprintf(
-				'<p><button name="%1$s" id="cc-btn" class="republication-tracker-tool-button page" onclick="window.location.href=\'%2$s\'">%1$s</button></p>',
+				'<p><button name="%1$s" id="cc-btn" class="republication-tracker-tool-button page" onclick="window.location.href=\'%2$s\'" role="link">%1$s</button></p>',
 				esc_html__( 'Republish This Story', 'republication-tracker-tool' ),
 				esc_url( '/republish' . $_SERVER['REQUEST_URI'] )
 			);
