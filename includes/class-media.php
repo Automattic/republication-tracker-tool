efa125ab25f457b25f56cdbddb06c48530fb9763
Takshil Kunadia <takshilkunadia@gmail.com>
2025-06-28 11:57:09 +0530

    feat: add bypass logic for global distribution check

--- includes/class-media.php
+++ includes/class-media.php
@@ -16,6 +16,7 @@ class Republication_Tracker_Tool_Media {
 	 * Should the media element be distributed?
 	 *
 	 * @param int $media_id ID of the media element.
+	 * @return bool True if the media can be distributed, false otherwise.
 	 */
 	public static function can_distribute( $media_id ) {
 		$media_distribution = get_option( 'republication_tracker_tool_media_distribution', 'on' );
@@ -24,8 +25,18 @@ class Republication_Tracker_Tool_Media {
 			return true;
 		}
 		// Otherwise, check the individual setting.
+		return self::get_can_distribute_meta( $media_id );
+	}
+
+	/**
+	 * Get the meta value that indicates if the media can be distributed.
+	 *
+	 * @param int $media_id ID of the media element.
+	 * @return bool True if the media can be distributed, false otherwise.
+	 */
+	public static function get_can_distribute_meta( $media_id ) {
 		if ( class_exists( '\Newspack\Newspack_Image_Credits' ) ) {
-			return (bool) get_post_meta( $media_id, \Newspack\Newspack_Image_Credits::MEDIA_CREDIT_CAN_DISTRIBUTE_META, true );
+			return get_post_meta( $media_id, \Newspack\Newspack_Image_Credits::MEDIA_CREDIT_CAN_DISTRIBUTE_META, true );
 		}
 
 		return false;
