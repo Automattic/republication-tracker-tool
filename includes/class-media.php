f077eb3e1e5931f2c130b11afd80436860f3594b
Takshil Kunadia <takshilkunadia@gmail.com>
2025-06-28 12:19:07 +0530

    refactor: media distribution logic to separate concerns

--- includes/class-media.php
+++ includes/class-media.php
@@ -19,26 +19,20 @@ class Republication_Tracker_Tool_Media {
 	 * @return bool True if the media can be distributed, false otherwise.
 	 */
 	public static function can_distribute( $media_id ) {
-		$media_distribution = get_option( 'republication_tracker_tool_media_distribution', 'on' );
-		if ( $media_distribution === 'on' ) {
-			// All media should be distributed, regardless of the individual setting.
-			return true;
-		}
-		// Otherwise, check the individual setting.
-		return self::get_can_distribute_meta( $media_id );
-	}
-
-	/**
-	 * Get the meta value that indicates if the media can be distributed.
-	 *
-	 * @param int $media_id ID of the media element.
-	 * @return bool True if the media can be distributed, false otherwise.
-	 */
-	public static function get_can_distribute_meta( $media_id ) {
 		if ( class_exists( '\Newspack\Newspack_Image_Credits' ) ) {
-			return get_post_meta( $media_id, \Newspack\Newspack_Image_Credits::MEDIA_CREDIT_CAN_DISTRIBUTE_META, true );
+			return (bool) get_post_meta( $media_id, \Newspack\Newspack_Image_Credits::MEDIA_CREDIT_CAN_DISTRIBUTE_META, true );
 		}
 
 		return false;
+	}
+
+	/**
+	 * Check if global media distribution is enabled.
+	 *
+	 * @return bool True if global distribution is enabled, false otherwise.
+	 */
+	public static function is_global_distribution_enabled() {
+		$media_distribution = get_option( 'republication_tracker_tool_media_distribution', 'on' );
+		return 'on' === $media_distribution;
 	}
 }
