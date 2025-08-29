9dc56eec8a67b9aacc783010b2307fb7cb0c43f2
matticbot <semantic-release-bot@martynus.net>
2025-08-29 20:02:51 +0000

    chore(release): 2.6.0-alpha.2 [skip ci]
    # [2.6.0-alpha.2](https://github.com/Automattic/republication-tracker-tool/compare/v2.6.0-alpha.1...v2.6.0-alpha.2) (2025-08-29)
    
    ### Features
    
    * gracefully support no licenses ([#270](https://github.com/Automattic/republication-tracker-tool/issues/270)) ([733b088](https://github.com/Automattic/republication-tracker-tool/commit/733b08899c487c6d058c5c95755516b39cda1a6c))

--- republication-tracker-tool.php
+++ republication-tracker-tool.php
@@ -6,7 +6,7 @@
  * Author URI:      https://labs.inn.org
  * Text Domain:     republication-tracker-tool
  * Domain Path:     /languages
- * Version:         2.6.0-alpha.1
+ * Version:         2.6.0-alpha.2
  *
  * @package         Republication_Tracker_Tool
  */
